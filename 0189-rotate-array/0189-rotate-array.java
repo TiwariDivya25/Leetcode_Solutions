5d6b632367efbab508983cc83ebab6a2b3c9e930
Divya Tiwari <divyat.social@gmail.com>
2025-12-18 21:54:26 +0530

    Time: 3 ms (7.55%), Space: 61.4 MB (22.22%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,23 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        for(int i = 0; i < nums.length/2; i++){
+        int n = nums.length;
+
+        if (n <= 1) return;
+        k = k % n;
+        for(int i = 0; i < n / 2; i++){
             int temp = nums[i];
-            nums[i] = nums[nums.length-i-1];
-            nums[nums.length-i-1] = temp;
+            nums[i] = nums[n - i - 1];
+            nums[n - i - 1] = temp;
         }
-        for(int i = 0; i < k/2; i++){
+        for(int i = 0; i < k / 2; i++){
             int temp = nums[i];
-            nums[i] = nums[k-i-1];
-            nums[k-i-1] = temp;
+            nums[i] = nums[k - i - 1];
+            nums[k - i - 1] = temp;
         }
-        for(int i = 0; i < (nums.length-k)/2; i++) {
-            int temp = nums[k+i];
-            nums[k+i] = nums[nums.length-i-1];
-            nums[nums.length-i-1] = temp;
-        } 
+        for(int i = 0; i < (n - k) / 2; i++){
+            int temp = nums[k + i];
+            nums[k + i] = nums[n - i - 1];
+            nums[n - i - 1] = temp;
+        }
     }
 }
