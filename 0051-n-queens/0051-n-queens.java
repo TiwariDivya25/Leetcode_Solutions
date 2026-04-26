1243cb575fc2d46e352a8465258ec5dc0823b655
Divya Tiwari <divyat.social@gmail.com>
2026-04-26 19:37:25 +0530

    Time: 5 ms (25.43%), Space: 46.5 MB (82.18%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<String>> ans;
+    public List<List<String>> solveNQueens(int n) {
+        ans = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        List<int[]> queens = new ArrayList<>();
+        dfs(board, 0, queens);
+        return ans;
+    }
+
+    private void dfs(char[][] board, int r, List<int[]> queens) {
+        if(queens.size() == board.length) {
+            List<String> rows = new ArrayList<>();
+            for(char[] row : board) {
+                rows.add(new String(row));
+            }
+            ans.add(rows);
+        }
+        for(int c = 0; c < board.length; c++) {
+            if(canAddQ(r, c, queens)) {
+                board[r][c] = 'Q';
+                queens.add(new int[] {r, c});
+                dfs(board, r+1, queens);
+                board[r][c] = '.';
+                queens.remove(queens.size()-1);
+            }
+        }
+    }
+    private boolean canAddQ(int r, int c, List<int[]> queens) {
+        for(int[] q : queens) {
+            int dx = Math.abs(r-q[0]);
+            int dy = Math.abs(c-q[1]);
+            if(dx==0||dy==0||dx==dy) return false;
+        }
+        return true;
+    }
+
+}
