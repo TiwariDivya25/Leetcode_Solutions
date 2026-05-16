9695ed379d558171bc6e164bb152f22fc2c2d622
Divya Tiwari <divyat.social@gmail.com>
2026-05-16 21:37:42 +0530

    Time: 2094 ms (34.29%), Space: 41.9 MB (89.93%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int n = 0;
+        for(int i = 1; i < num; i++) { 
+            if(num % i == 0) { 
+                n += i;
+            }
+        }
+        return n == num;
+    }
+}
