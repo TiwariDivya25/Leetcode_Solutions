0e5b85fc1f3ced9fec44a88e5c5615b04b15430a
Divya Tiwari <divyat.social@gmail.com>
2026-05-31 23:18:04 +0530

    Time: 26 ms (81.52%), Space: 107.4 MB (65.13%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long currMass = mass;
+        for(int a : asteroids) {
+            if(currMass < a) return false;
+            else currMass += a;
+        }
+        return true;
+    }
+}
